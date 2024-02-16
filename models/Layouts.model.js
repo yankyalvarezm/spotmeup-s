bacb6d109e038e365e7fbd6488f8aadd5ddcc541
alextor26 <alextor2625@gmail.com>
2024-02-16 16:45:12 -0400

    Refactored some layout properties

--- models/Layouts.model.js
+++ models/Layouts.model.js
@@ -3,10 +3,12 @@ const { Schema, model } = require("mongoose");
 const layoutSchema = new Schema(
   {
     name: String,
-    x: Number,
-    y: Number,
-    width: Number,
-    height: Number,
+    width: { type: Number, default: 400 },
+    maxWidth: {type: Number, default: 450},
+    height: { type: Number, default: 400 },
+    maxHeight: {type: Number, default: 600},
+    borderSize: { type: Number, default: 2 },
+    borderRadius: { type: Number, default: 2 },
     status: String,
     capacity: Number,
     blocks: [{ type: Schema.Types.ObjectId, ref: "Blocks" }],
