fe80c3f3fef3edb9e18cfe66eea73f1cfa39eacc
yankyalvarezm <alvarezyanky7#gmail.com>
2024-02-19 15:05:52 -0500

    added x and y to the model

--- models/Layouts.model.js
+++ models/Layouts.model.js
@@ -3,12 +3,14 @@ const { Schema, model } = require("mongoose");
 const layoutSchema = new Schema(
   {
     name: String,
-    width: { type: Number, default: 400 },
-    maxWidth: {type: Number, default: 450},
+    width: { type: Number, default: 800 },
+    maxWidth: {type: Number, default: 1500},
     height: { type: Number, default: 400 },
     maxHeight: {type: Number, default: 600},
     borderSize: { type: Number, default: 2 },
-    borderRadius: { type: Number, default: 2 },
+    borderRadius: { type: Number, default: 0 },
+    x: { type: Number, default: 50 },
+    y: { type: Number, default: 50 },
     status: String,
     capacity: Number,
     blocks: [{ type: Schema.Types.ObjectId, ref: "Blocks" }],
