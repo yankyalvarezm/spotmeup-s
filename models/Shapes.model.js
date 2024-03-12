a3f2e15d5439bc07f04d4bad065e6cf6a052defc
alextor26 <alextor2625@gmail.com>
2024-03-11 21:02:20 -0400

    Added 2 properties to Shapes model and added trim to String properties

--- models/Shapes.model.js
+++ models/Shapes.model.js
@@ -2,22 +2,24 @@ const { Schema, model } = require("mongoose");
 
 const shapeModel = new Schema(
   {
-    shapeType: String,
-    name: String,
+    shapeType: {type:String, trim:true},
+    name: {type:String, default:""},
     width: {type: Number, default: 100},
     height: {type: Number, default: 100},
-    border: String,
+    border: {type:String, trim:true},
     borderRadius: Number,
     borderLeft: Number,
     borderRight: Number,
     borderTop: Number,
     borderBottom: Number,
-    borderColor: {type: String, default: "black"},
+    borderColor: {type: String, default: "black",trim:true},
     borderSize: {type:Number, default: 1},
-    color: {type: String, default: "white"},
-    backgroundColor: {type: String, default: "black"},
+    color: {type: String, default: "white", trim:true},
+    backgroundColor: {type: String, default: "black", trim:true},
     x: {type: Number, default:0},
     y: {type: Number, default:0},
+    justifyContent: {type: String, default:"", trim:true},
+    alignItems: {type: String, default:"", trim:true},
     layout: {type: Schema.Types.ObjectId, ref:"Layouts"}
   },
   {
