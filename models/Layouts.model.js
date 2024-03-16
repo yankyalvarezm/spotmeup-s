769409686047771f3a5b9df60b6bad5e054bd008
yankyalvarezm <alvarezyanky7#gmail.com>
2024-03-15 22:46:54 -0400

    fixed dynamic shape logic

--- models/Layouts.model.js
+++ models/Layouts.model.js
@@ -4,9 +4,9 @@ const layoutSchema = new Schema(
   {
     name: String,
     width: { type: Number, default: 800 },
-    maxWidth: {type: Number, default: 500},
+    maxWidth: {type: Number, default: 1200},
     height: { type: Number, default: 400 },
-    maxHeight: {type: Number, default: 600},
+    maxHeight: {type: Number, default: 1600},
     borderSize: { type: Number, default: 2 },
     borderRadius: { type: Number, default: 0 },
     x: { type: Number, default: 50 },
